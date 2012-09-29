6d3df9eeb66ca4ea3b68605d4e28d31e1273bd90
Alex Rudy <alex.rudy@gmail.com>
2012-09-29 11:30:56 -0700

    Basic program structure setup
    - Module folder
    - setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 
+#  setup.py
+#  pyshell
+#  
+#  Created by Alexander Rudy on 2012-09-29.
+#  Copyright 2012 Alexander Rudy. All rights reserved.
+# 
+
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+from pyshell.version import version
+
+setup(
+    name = 'pyshell',
+    version = version,
+    packages = find_packages(exclude=['tests'])
+    package_data = {'pyshell':['Defaults.yaml']}
+    install_requires = ['distribute','PyYAML>=3.10'],
+    test_suite = 'tests',
+    author = 'Alexander Rudy',
+    author_email = 'dev@alexrudy.org'
+)
