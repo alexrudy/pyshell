b7a33e7d7a7a9e3bba55df3ee221526ada606c6c
Alex Rudy <alex.rudy@gmail.com>
2012-10-23 22:46:52 -0700

    util now complies with pylint

--- pyshell/util.py
+++ pyshell/util.py
@@ -6,29 +6,44 @@
 #  Created by Jaberwocky on 2012-10-16.
 #  Copyright 2012 Jaberwocky. All rights reserved.
 # 
+"""
+:mod:`util` - Utilities
+-----------------------
 
+..autofunction: force_dir_path
+
+..autofunction: query_yes_no
+
+..autofunction: query_string
+
+"""
 import os
 import sys
 
 def force_dir_path(path):
-    """Force the input path to be a directory."""
+    """Force the input path to be a directory.
+    
+    Paths are normalized, and then it is ensured that the
+    path ends with a `/`.
+    
+    Converts `/some//path/here` to `/some/path/here/` and
+    `/some//path/here//` to `/some/path/here/`
+    """
     path = os.path.normpath(path)
-    if path.endswith("/"):
-        return path.rstrip("/") + "/"
-    else:
-        return path + "/"
-      
+    return path.rstrip("/") + "/"
+    
+
 # Borrowed from:
 # http://stackoverflow.com/questions/3041986/python-command-line-yes-no-input  
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
 
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
+    :param question: A string presented to the user
+    :param default: The answer if the user hits <Enter> with no input.
         It must be "yes" (the default), "no" or None (meaning
         an answer is required of the user).
-
-    The "answer" return value is one of "yes" or "no".
+    :return: `True` or `False` for "yes" or "no" respectively.
+    
     """
     valid = {"yes":True,   "y":True,  "ye":True,
              "no":False,     "n":False}
@@ -52,8 +67,14 @@ def query_yes_no(question, default="yes"):
             sys.stdout.write("Please respond with 'yes' or 'no' "\
                              "(or 'y' or 'n').\n")
                              
-def query_string(question,default=None,validate=None):
-    """Ask a question, evaluating to a single string."""
+def query_string(question, default=None, validate=None):
+    """Ask a question via raw_input, and return the answer as a string.
+    
+    :param question: A string presented to the user
+    :param default: The answer if the user hits <Enter> with no input.
+    :return: A string with the user's answer or the default if no answer.
+    
+    """
     
     if default is None:
         prompt = " :"
