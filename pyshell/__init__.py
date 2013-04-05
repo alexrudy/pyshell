70bc0c4070157f02a65e32223d164890184d9c8c
Alex Rudy <alex.rudy@gmail.com>
2013-04-05 14:57:47 -0700

    [DEV] Compliance with pylint

--- pyshell/__init__.py
+++ pyshell/__init__.py
@@ -5,9 +5,17 @@
 #  Created by Alexander Rudy on 2012-09-29.
 #  Copyright 2012 Alexander Rudy. All rights reserved.
 # 
+"""
+PyShell master module
+=====================
+
+Use this for quick access to commonly used classes!
+"""
+
+# pylint: disable = invalid-name
 
 version = "0.2.0"
 
 from .base import CLIEngine
 from .subcommand import SCEngine, SCController
-__all__ = ['CLIEngine','SCEngine','SCController']
+__all__ = ['CLIEngine', 'SCEngine', 'SCController']
