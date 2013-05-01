6dccfad235b576218ee73819e506e87ec1ed0fa1
Alex Rudy <alex.rudy@gmail.com>
2013-04-30 21:32:25 -0700

    [DEV] Better pyshell __all__ items

--- pyshell/__init__.py
+++ pyshell/__init__.py
@@ -15,10 +15,17 @@ Use this for quick access to commonly used classes!
 # pylint: disable = invalid-name
 
 version = "0.2.0"
+__all__ = []
 
 from .base import *
 import base
+__all__ += base.__all__
+
+from loggers import getLogger, getSimpleLogger
+__all__ += ['getLogger' , 'getSimpleLogger']
+
 import loggers
 loggers.buffer_logger()
+
 from .subcommand import SCEngine, SCController
-__all__ = ['SCEngine', 'SCController'] + base.__all__
+__all__ += ['SCEngine', 'SCController']
