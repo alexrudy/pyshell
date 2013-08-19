23313431ecd75014be1d6b5c416cedd44075d5e0
Alex Rudy <alex.rudy@gmail.com>
2013-08-18 20:49:26 -0600

    [DOC] Release Notes and version bump

--- pyshell/__init__.py
+++ pyshell/__init__.py
@@ -17,7 +17,7 @@ from __future__ import (absolute_import, unicode_literals, division,
 
 # pylint: disable = invalid-name
 
-version = "0.4.0"
+version = "0.4.1"
 __all__ = []
 
 from .base import *
