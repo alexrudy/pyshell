c2386a29742244a7b4f65db15e655074d0e8ddcc
Alex Rudy <alex.rudy@gmail.com>
2013-08-18 18:49:45 -0600

    [DEV] Version bumping now fully supported

--- pyshell/bump_version.py
+++ pyshell/bump_version.py
@@ -6,15 +6,62 @@
 #  Created by Alexander Rudy on 2013-01-12.
 #  Copyright 2013 Alexander Rudy. All rights reserved.
 # 
-from __future__ import division
 """
 bump_version -- Increase version numbers!
 """
+from __future__ import division
 from .base import CLIEngine
+import glob, os, os.path, shutil, tempfile
+import re
 
 class VersionBumper(CLIEngine):
     """Bump the version of your source code!"""
-    def __init__(self, arg):
-        super(VersionBumper, self).__init__()
-        self.arg = arg
+    
+    defaultcfg = "bump_version.yml"
+    
+    def init(self):
+        """docstring for init"""
+        super(VersionBumper, self).init()
+        self.parser.add_argument('-C','--cwd',type=unicode,dest='cwd',help="Set path",default=os.getcwd())
+        self.parser.add_argument('--no-tmp',action="store_false", dest='tmp', help="Use temporary files or backups?")
+    
+    def after_configure(self):
+        """Setup init"""
+        super(VersionBumper, self).after_configure()
+        self.parser.add_argument('version',type=unicode,help="New version string to use.")
+        self.parser.add_argument('--glob',type=unicode,help="File pattern glob for changing versions.",
+            default=self.config["glob.files"])
+        self.parser.add_argument('--match',type=unicode,help="Match Regex",default=self.config["regex.find"],
+            metavar="regexp")
+        self.parser.add_argument('--replace',type=unicode,help="Replace Regex",default=self.config["regex.replace"],
+            metavar="\"{version}\"")
+            
+        
+    def do(self):
+        """Do the actual parsing."""
+        version_find = re.compile(self.opts.match.rstrip("\n"))
+        version_replace = self.opts.replace.rstrip("\n").format(version=self.opts.version)
+        filenames = glob.glob(os.path.join(self.opts.cwd,self.opts.glob))
+        if self.opts.tmp:
+            filepairs = [(filename, tempfile.mkstemp()[1]) for filename in filenames]
+        else:
+            filepairs = [(filename, filename+".backup") for filename in filenames]
+        try:
+            for filename, newname in filepairs:
+                with open(filename,'r') as filestream:
+                    with open(newname,'w') as outstream:
+                        write = outstream.write
+                        sub = re.sub
+                        for num, line in enumerate(filestream):
+                            result, = sub(version_find,version_replace,line),
+                            write(result)
+        except Exception as e:
+            raise
+        else:
+            for filename, newname in filepairs:
+                shutil.copy(newname, filename)
+        finally:
+            for filename,newname in filepairs:
+                if os.path.exists(newname):
+                    os.remove(newname)
         
