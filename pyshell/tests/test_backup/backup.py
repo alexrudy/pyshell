208c59384e7c5cf7d556e038be37187248c18b29
Alex Rudy <alex.rudy@gmail.com>
2013-11-15 16:08:34 -0800

    Adding backup.py test script

---
+++ pyshell/tests/test_backup/backup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# 
+#  backup.py
+#  pyshell
+#  
+#  Created by Jaberwocky on 2013-11-15.
+#  Copyright 2013 Jaberwocky. All rights reserved.
+# 
+
+from pyshell.backup import BackupEngine
+
+if __name__ == '__main__':
+    BackupEngine.script()
