b0cffb0bf78534b31e3ab4ce03363cfdf5c71e6c
Alex Rudy <alex.rudy@gmail.com>
2013-11-15 16:09:30 -0800

    Fixed a test run in python3

--- pyshell/tests/test_yaml.py
+++ pyshell/tests/test_yaml.py
@@ -14,6 +14,7 @@ import nose.tools as nt
 import os
 import collections
 from six.moves import cStringIO as StringIO
+import six
 
 class test_yaml(object):
     """pyshell.yaml"""
@@ -31,7 +32,7 @@ class test_yaml(object):
         data = yaml.dump(self.test_dict_A, Dumper=ps_yaml.UnicodeDumper)
         test_A = yaml.load(data, Loader=ps_yaml.UnicodeLoader)
         for key in test_A.keys():
-            nt.ok_(isinstance(key, unicode),"Key was not UNICODE! {}".format(type(key)))
+            nt.ok_(isinstance(key, six.text_type),"Key was not UNICODE! {}".format(type(key)))
     
     def test_roundtrip_order(self):
         """Test the order of everything."""
